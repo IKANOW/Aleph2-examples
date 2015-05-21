570a19add23d8404b7503060bab193784b571c9b
Alex Piggott <apiggott@ikanow.com>
2015-05-21 16:29:01 -0400

    ALEPH-22 #comment Just returned sample basic message bean from the example version

--- aleph2_empty_examples/src/com/ikanow/aleph2/test/example/ExampleHarvestTechnology.java
+++ aleph2_empty_examples/src/com/ikanow/aleph2/test/example/ExampleHarvestTechnology.java
@@ -1,5 +1,6 @@
 package com.ikanow.aleph2.test.example;
 
+import java.util.Date;
 import java.util.concurrent.CompletableFuture;
 
 import com.ikanow.aleph2.data_model.interfaces.data_import.IHarvestContext;
@@ -16,48 +17,129 @@ public class ExampleHarvestTechnology implements IHarvestTechnologyModule {
 
 	public CompletableFuture<BasicMessageBean> onNewSource(
 			DataBucketBean new_bucket, IHarvestContext context) {
-		return null;
+		return CompletableFuture.completedFuture(
+				new BasicMessageBean(
+						new Date(), // date
+						true, // success
+						new_bucket.display_name(),
+						"onNewSource",
+						null, // message code
+						"called onNewSource",
+						null // details
+						));
 	}
 
 	public CompletableFuture<BasicMessageBean> onUpdatedSource(
 			DataBucketBean old_bucket, DataBucketBean new_bucket,
 			IHarvestContext context) {
-		return null;
+		return CompletableFuture.completedFuture(
+				new BasicMessageBean(
+						new Date(), // date
+						true, // success
+						new_bucket.display_name(),
+						"onUpdatedSource",
+						null, // message code
+						"called onUpdatedSource",
+						null // details
+						));
 	}
 
 	public CompletableFuture<BasicMessageBean> onSuspend(
 			DataBucketBean to_suspend, IHarvestContext context) {
-		return null;
+		return CompletableFuture.completedFuture(
+				new BasicMessageBean(
+						new Date(), // date
+						true, // success
+						to_suspend.display_name(),
+						"onSuspend",
+						null, // message code
+						"called onSuspend",
+						null // details
+						));
 	}
 
 	public CompletableFuture<BasicMessageBean> onResume(
 			DataBucketBean to_resume, IHarvestContext context) {
-		return null;
+		return CompletableFuture.completedFuture(
+				new BasicMessageBean(
+						new Date(), // date
+						true, // success
+						to_resume.display_name(),
+						"onResume",
+						null, // message code
+						"called onResume",
+						null // details
+						));
 	}
 
 	public CompletableFuture<BasicMessageBean> onPurge(DataBucketBean to_purge,
 			IHarvestContext context) {
-		return null;
+		return CompletableFuture.completedFuture(
+				new BasicMessageBean(
+						new Date(), // date
+						true, // success
+						to_purge.display_name(),
+						"onPurge",
+						null, // message code
+						"called onPurge",
+						null // details
+						));
 	}
 
 	public CompletableFuture<BasicMessageBean> onDelete(
 			DataBucketBean to_delete, IHarvestContext context) {
-		return null;
+		return CompletableFuture.completedFuture(
+				new BasicMessageBean(
+						new Date(), // date
+						true, // success
+						to_delete.display_name(),
+						"onDelete",
+						null, // message code
+						"called onDelete",
+						null // details
+						));
 	}
 
 	public CompletableFuture<BasicMessageBean> onPeriodicPoll(
 			DataBucketBean polled_bucket, IHarvestContext context) {
-		return null;
+		return CompletableFuture.completedFuture(
+				new BasicMessageBean(
+						new Date(), // date
+						true, // success
+						polled_bucket.display_name(),
+						"onPeriodicPoll",
+						null, // message code
+						"called onPeriodicPoll",
+						null // details
+						));
 	}
 
 	public CompletableFuture<BasicMessageBean> onHarvestComplete(
 			DataBucketBean completed_bucket, IHarvestContext context) {
-		return null;
+		return CompletableFuture.completedFuture(
+				new BasicMessageBean(
+						new Date(), // date
+						true, // success
+						completed_bucket.display_name(),
+						"onHarvestComplete",
+						null, // message code
+						"called onHarvestComplete",
+						null // details
+						));
 	}
 
 	public CompletableFuture<BasicMessageBean> onTestSource(
 			DataBucketBean test_bucket, ProcessingTestSpecBean test_spec,
 			IHarvestContext context) {
-		return null;
+		return CompletableFuture.completedFuture(
+				new BasicMessageBean(
+						new Date(), // date
+						true, // success
+						test_bucket.display_name(),
+						"onTestSource",
+						null, // message code
+						"called onTestSource: " + test_spec == null ? "no test spec" : (test_spec.requested_num_objects() + " / " + test_spec.max_run_time_secs()),
+						null // details
+						));
 	}
 }
