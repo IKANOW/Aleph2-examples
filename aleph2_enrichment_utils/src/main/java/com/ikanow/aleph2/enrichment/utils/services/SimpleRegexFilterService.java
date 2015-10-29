4f20e872822601729fa4288c124084a261642a9f
Alex-Ikanow <apiggott@ikanow.com>
2015-10-29 17:15:25 -0400

    ALEPH-12 Tweak to batch enrichment interface to do a better job of identifying location in chain, in case it matters

--- aleph2_enrichment_utils/src/main/java/com/ikanow/aleph2/enrichment/utils/services/SimpleRegexFilterService.java
+++ aleph2_enrichment_utils/src/main/java/com/ikanow/aleph2/enrichment/utils/services/SimpleRegexFilterService.java
@@ -24,7 +24,9 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 
+
 import scala.Tuple2;
+
 
 
 import com.fasterxml.jackson.databind.JsonNode;
@@ -64,7 +66,8 @@ public class SimpleRegexFilterService implements IEnrichmentBatchModule {
 	@Override
 	public void onStageInitialize(IEnrichmentModuleContext context,
 			DataBucketBean bucket, EnrichmentControlMetadataBean control,
-			boolean final_stage) {
+			final Tuple2<ProcessingStage, ProcessingStage> previous_next)
+	{
 		
 		final SimpleRegexFilterBean config_bean = BeanTemplateUtils.from(Optional.ofNullable(control.config()).orElse(Collections.emptyMap()), SimpleRegexFilterBean.class).get();
 		
