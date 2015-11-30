cef3265db8bce22851ce3d997af7fe6916e11ddf
jfreydank <jfreydank@ikanow.com>
2015-11-30 17:55:36 -0600

    Created WebLoginSecurityModule for innjectingr realms into websecurityManager.

--- aleph2_web_login/src/main/java/com/ikanow/aleph2/web_login/WebEnvironment.java
+++ aleph2_web_login/src/main/java/com/ikanow/aleph2/web_login/WebEnvironment.java
@@ -1,7 +1,46 @@
 package com.ikanow.aleph2.web_login;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.apache.shiro.realm.Realm;
 import org.apache.shiro.web.env.IniWebEnvironment;
+import org.apache.shiro.web.filter.mgt.FilterChainResolver;
+import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
+import org.apache.shiro.web.mgt.WebSecurityManager;
+
+import com.google.inject.Inject;
 
 public class WebEnvironment extends IniWebEnvironment{
+
+    protected void configure() {
+
+        this.objects.clear();
+
+        WebSecurityManager securityManager = createWebSecurityManager();
+        setWebSecurityManager(securityManager);
+
+        FilterChainResolver resolver = createFilterChainResolver();
+        if (resolver != null) {
+            setFilterChainResolver(resolver);
+        }
+    }
+
+	@Override
+	protected WebSecurityManager createWebSecurityManager() {
+		WebSecurityManager webSecurityManager = super.createWebSecurityManager();
+		if(webSecurityManager instanceof DefaultWebSecurityManager){
+			DefaultWebSecurityManager securityManager = (DefaultWebSecurityManager)webSecurityManager;
+			Collection<Realm> realms = getRealms();
+			securityManager.setRealms(realms);
+		}
+		return webSecurityManager;
+	}
+
+	//TODO get from injector
+	private Collection<Realm> getRealms() {
+		Collection<Realm> realms = new ArrayList();
+		return realms;
+	}
 
 }
