69154bf11ea58c0603ebd59c9a56e58205bfa25e
jfreydank <jfreydank@ikanow.com>
2015-12-03 12:08:39 -0600

    Added realm injection to Weblogin.

--- aleph2_web_login/src/main/java/com/ikanow/aleph2/web_login/WebEnvironment.java
+++ aleph2_web_login/src/main/java/com/ikanow/aleph2/web_login/WebEnvironment.java
@@ -1,29 +1,38 @@
 package com.ikanow.aleph2.web_login;
 
-import java.util.ArrayList;
 import java.util.Collection;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.apache.shiro.realm.Realm;
 import org.apache.shiro.web.env.IniWebEnvironment;
-import org.apache.shiro.web.filter.mgt.FilterChainResolver;
 import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
 import org.apache.shiro.web.mgt.WebSecurityManager;
 
-import com.google.inject.Inject;
+import com.google.inject.Injector;
+import com.ikanow.aleph2.data_model.utils.ModuleUtils;
+import com.ikanow.aleph2.security.module.CoreSecurityModule;
+
 
 public class WebEnvironment extends IniWebEnvironment{
 
+	private static final Logger logger = LogManager.getLogger();
+
+
+	protected Collection<Realm> realms = null;
+
     protected void configure() {
+    	
+		try {
+			Injector injector = ModuleUtils.getAppInjector().get();			
+			this.realms = injector.getInstance(CoreSecurityModule.realmCollectionKey());
+			//logger.debug("Realm:"+realms);
+		} catch (Throwable e) {
+			logger.error("Caught exception getting app injector:",e);
+		}
 
-        this.objects.clear();
-
-        WebSecurityManager securityManager = createWebSecurityManager();
-        setWebSecurityManager(securityManager);
-
-        FilterChainResolver resolver = createFilterChainResolver();
-        if (resolver != null) {
-            setFilterChainResolver(resolver);
-        }
+    	super.configure();
+        
     }
 
 	@Override
@@ -31,16 +40,12 @@ public class WebEnvironment extends IniWebEnvironment{
 		WebSecurityManager webSecurityManager = super.createWebSecurityManager();
 		if(webSecurityManager instanceof DefaultWebSecurityManager){
 			DefaultWebSecurityManager securityManager = (DefaultWebSecurityManager)webSecurityManager;
-			Collection<Realm> realms = getRealms();
 			securityManager.setRealms(realms);
 		}
 		return webSecurityManager;
 	}
 
-	//TODO get from injector
-	private Collection<Realm> getRealms() {
-		Collection<Realm> realms = new ArrayList();
-		return realms;
-	}
-
+	
+	
+	
 }
