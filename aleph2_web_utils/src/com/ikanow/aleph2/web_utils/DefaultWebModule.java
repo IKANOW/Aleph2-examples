e51930fa76ddc11dde7e9e60d23dfd724d2435c9
jfreydank <jfreydank@ikanow.com>
2016-01-28 18:14:38 -0600

    Changes for web_sso but using guice for some services.

--- aleph2_web_utils/src/com/ikanow/aleph2/web_utils/DefaultWebModule.java
+++ aleph2_web_utils/src/com/ikanow/aleph2/web_utils/DefaultWebModule.java
@@ -1,13 +1,27 @@
 package com.ikanow.aleph2.web_utils;
 
+import javax.servlet.ServletContext;
+
 import com.google.inject.AbstractModule;
 
 public class DefaultWebModule extends AbstractModule {
 
+	
+	protected ServletContext servletContext;
+
+	public ServletContext getServletContext() {
+		return servletContext;
+	}
+
+	public DefaultWebModule(ServletContext sc) {
+		this.servletContext = sc;
+    }
+
 	@Override
 	protected void configure() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
+	
 }
