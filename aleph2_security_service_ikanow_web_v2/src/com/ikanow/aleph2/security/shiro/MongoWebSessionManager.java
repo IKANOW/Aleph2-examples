b1df36fac4096bd38b07488a1dcd26b45fb7cc1c
jfreydank <jfreydank@ikanow.com>
2016-03-31 18:50:18 -0500

    Cookie changes.

--- aleph2_security_service_ikanow_web_v2/src/com/ikanow/aleph2/security/shiro/MongoWebSessionManager.java
+++ aleph2_security_service_ikanow_web_v2/src/com/ikanow/aleph2/security/shiro/MongoWebSessionManager.java
@@ -2,6 +2,8 @@ package com.ikanow.aleph2.security.shiro;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.shiro.web.servlet.Cookie;
+import org.apache.shiro.web.servlet.SimpleCookie;
 import org.apache.shiro.web.session.mgt.DefaultWebSessionManager;
 
 import com.google.inject.Inject;
@@ -10,12 +12,20 @@ import com.google.inject.Inject;
 
 public class MongoWebSessionManager extends DefaultWebSessionManager {
 	protected static Logger logger = LogManager.getLogger(MongoWebSessionManager.class);
-
+	
+	public static String COOKIE_NAME = "ALEPH2_SESSION";
 	@Inject
 	public MongoWebSessionManager(MongoDbSessionDao mongoSessionDao){
-		super();
 		logger.debug("MongoWebSessionManager created.");
 		setSessionDAO(mongoSessionDao);
+        Cookie cookie = new SimpleCookie(COOKIE_NAME);
+        cookie.setHttpOnly(true); //more secure, protects against XSS attacks
+        long sessionTimeout = getGlobalSessionTimeout();
+        if(sessionTimeout>0){
+        	cookie.setMaxAge((int)(sessionTimeout/1000));
+        }
+        setSessionIdCookie(cookie);
+        setSessionIdCookieEnabled(true);
 	}
 
 }
